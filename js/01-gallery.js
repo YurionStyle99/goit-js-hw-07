b05cf0da90faf9959c317f570c3c1244c2b50a38
YurionStyle <74459224+YurionStyle@users.noreply.github.com>
2023-02-28 18:05:03 +0100

    fix0.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,39 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector(".gallery");
-
-
 const createEL = createCards(galleryItems);
 
 gallery.insertAdjacentHTML('beforeend', createEL)
-card.addEventListener('click', onClick)
-gallery.addEventListener('click', close)
 
 function createCards(){
   return galleryItems.map(({original,preview,description}) => {
 return`
 <div class="gallery__item">
-<a href="${original}" class="gallery__link"  rel = ”noreferrer noopener”>
+<a href="${original}" class="gallery__link"  rel = "noreferrer noopener">
 <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
 </a>
 </div>
 `
-
-
 }).join("")
 }
-
 
 gallery.innerHTML = createEL;
 
 gallery.addEventListener('click', onLinkClick);
 
 function onLinkClick(event) {
-    event.preventDefault();
-	const instance = basicLightbox.create(`
-		<img width="1400" height="900" src="${event.currentTarget.dataset.source}">
-	`)
-	instance.show()
+  event.preventDefault();
+  const instance = basicLightbox.create(`
+    <img width="1400" height="900" src="${event.target.dataset.source}">
+  `)
+  instance.show();
+  instance.element().addEventListener('keydown', onEscPress);
+
+  function onEscPress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
 }
+
+
 
 console.log(galleryItems);
